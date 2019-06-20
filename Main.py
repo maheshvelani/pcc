3ae02c06e26bfbeb8661960a37b842c390ca5683
maheshvelani <vmahesh@invader28>
2019-06-19 22:22:00 -0700

    Updated Main.py file

--- Main.py
+++ Main.py
@@ -15,20 +15,20 @@ class Main:
     def __init__(self):
          pass
 
-    @static_method
+    @staticmethod
     def get_current_time():
         return strftime("%Y-%m-%d_%H%M%S", gmtime())
     
 
-    @class_method
+    @classmethod
     def start_test_exec(cls):
         """ Trigger Test Execution and dump
             logs at appropriate location
         """
         time_str = cls.get_current_time()
-        os.system("robot -l ./logs/log_{0}.html -r ./logs/report_{0}.html -o ./logs/output_{0}.xml 
+        os.system("robot -l ./logs/log_{0}.html -r ./logs/report_{0}.html -o ./logs/output_{0}.xml \
                   ./test_suite/auto_pcc_regression_suite.robot".format(time_str))
 
 
-if __name___ == '__main__':
+if __name__ == '__main__':
     Main.start_test_exec()
