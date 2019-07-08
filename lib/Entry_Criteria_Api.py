e6a5a7193cc4693b57fafbd42ed258ace9339968
jskhedkarl <suraj.khedkar@calsoftinc.com>
2019-07-08 06:01:07 -0500

    Updated Entry Critearia API with backend cleanup

--- lib/Entry_Criteria_Api.py
+++ lib/Entry_Criteria_Api.py
@@ -6,15 +6,22 @@
 #
 ################################################
 
+import os
+import subprocess
+import time
 from robot.api import logger
+from robot.libraries.OperatingSystem import OperatingSystem
+from SSHLibrary import SSHLibrary
 
 
 def robot_logger(msg):
     """Custom logger
     """
-    logger.info(str(msg), html=True, also_console = True)
+    msg_str = "\n{0}\n".format(msg)
+    logger.info(str(msg_str), html=True, also_console = True)
 
-class Entry_Criteria_Api:
+
+class Entry_Criteria_Api(OperatingSystem, SSHLibrary):
 
     def get_available_node_data(self, resp_data):
         """Check any Invader or Server is available in node list
@@ -24,17 +31,146 @@ class Entry_Criteria_Api:
         node_id_lst = []
 
         try:
-            if resp_data['Data'] == None:
-                return False, None
+            if not resp_data['Data']:
+                return False, None, None
             else:
                 for index, data in enumerate(eval(str(resp_data))['Data']):
                     node_data.update({index:{"name":data['Name'], "ID" : str(data["Id"]), \
                                              "HOST" : str(data["Host"])}})
                     node_id_lst.append(int(data['Id']))
+                robot_logger("Available Node Data = {0}".format(str(node_data)))
                 return True, node_id_lst, node_data
-
-            return False, None
+            return False, None, None
         except Exception:
-            return False, None
+            return False, None, None
 
 
+    def get_node_type(self, node_data):
+        """ Get Node Type(Server Or Invader)
+        """
+        node_type_dict = {}
+
+        try:
+            for data in node_data:
+                cmd = "ssh-keygen -f \"/home/pcc/.ssh/known_hosts\" -R {0}".format(node_data[data]["HOST])
+                try:
+                    code, output = self.run_and_return_rc_and_output(cmd)
+                except:
+                    pass
+                robot_logger("cmd-{0} o/p={1}".format(str(cmd), output))
+
+                cmd = "ssh {0} \'{1}\'".format(node_data[data]["HOST"], "goes status")
+                try:
+                    code, output = self.run_and_return_rc_and_output(cmd)
+                except:
+                    pass
+                robot_logger("cmd-{0} o/p={1}".format(str(cmd), output))
+
+                if "goes: command not found" in output:
+                    robot_logger("Node {0} is Server".format(node_data[data]["name"]))
+                    node_type_dict.update({node_data[data]["HOST"] : "Server"})
+                elif "No route to host" in output:
+                    robot_logger("Node {0} is not connected with PCC".format(node_data[data]["name"]))
+                else:
+                    robot_logger("Node {0} is an Invader".format(node_data[data]["name"]))
+                    node_type_dict.update({node_data[data]["HOST"] : "Invader"})
+            return node_type_dict
+        except:
+            return node_type_dict
+
+    
+    def clean_invader(self, ip_addr):
+        """ Clean Invader from Backend
+        """
+        try:
+            login_op = self.open_connection(ip_addr)
+            robot_logger("login over invader Ip = ", ip)
+            self.login("pcc", "cals0ft")
+
+            out = self.execute_command("sudo -s")
+            robot_logger("cmd = {} and o/p = {1}".format("sudo -s", out))
+
+            out = self.execute_command("crontab -r")
+            robot_logger("cmd = {} and o/p = {1}".format("crontab -r", out))
+            time.sleep(1)
+
+            out = self.execute_command("ps -aef | grep supervisord | awk '{print $2}' | xargs kill -9")
+            robot_logger("cmd = {} and o/p = {1}".format("kill supervisord", out))
+            time.sleep(1)
+
+            out = self.execute_command("ps -aef | grep tinyproxy | awk '{print $2}' | xargs kill -9")
+            robot_logger("cmd = {} and o/p = {1}".format("kill tinyproxy", out))
+            time.sleep(1)
+                       
+            out = self.execute_command("ps -aef | grep dnsmasq | awk '{print $2}' | xargs kill -9")
+            robot_logger("cmd = {} and o/p = {1}".format("kill dnsmasq", out))                  
+            time.sleep(1)
+
+            out = self.execute_command("ps -aef | grep dnsmasq | awk '{print $2}' | xargs kill -9")
+            robot_logger("cmd = {} and o/p = {1}".format("kill dnsmasq", out))
+            time.sleep(1)
+                     
+            out = self.execute_command("ps -aef | grep pccagent | awk '{print $2}' | xargs kill -9")
+            robot_logger("cmd = {} and o/p = {1}".format("kill pccagent ", out))
+            time.sleep(1)
+
+            out = self.execute_command("ps -aef | grep collector | awk '{print $2}' | xargs kill -9")
+            robot_logger("cmd = {} and o/p = {1}".format("kill collector ", out))
+            time.sleep(1)
+
+            out = self.execute_command("service maas-ROOT stop")
+            robot_logger("cmd = {} and o/p = {1}".format("service maas-ROOT stop", out))
+            time.sleep(3)
+
+            out = self.execute_command("apt-get remove -y dnsmasq tinyproxy supervisor lighttpd")
+            robot_logger("cmd = {} and o/p = {1}".format("apt-get remove -y dnsmasq tinyproxy supervisor lighttpd", out))
+            time.sleep(10)
+                      
+            out = self.execute_command("rm -rf /srv/maas/")
+            robot_logger("cmd = {} and o/p = {1}".format("rm -rf /srv/maas/", out))
+            time.sleep(1)
+
+            out = self.execute_command("rm -rf /etc/network/interfaces.d/maas-xeth*")
+            robot_logger("cmd = {} and o/p = {1}".format("rm -rf /etc/network/interfaces.d/maas-xeth*", out))
+            time.sleep(1)           
+        except:
+              pass
+
+
+    def clear_server(self, ip_addr):
+        """ Clear server From Backend
+        """
+        cmd = "ssh {0} \'{1}\'".format((ip_addr, "sudo crontab -r")
+        try:
+            code, output = self.run_and_return_rc_and_output(cmd)
+        except:
+            pass
+        robot_logger("cmd-{0} o/p={1}".format(str(cmd), output))
+
+        cmd = "ssh {0} \'{1}\'".format((ip_addr, "sudo ps -aef | grep pccagent | awk '{print $2}' | xargs kill -9")
+        try:
+            code, output = self.run_and_return_rc_and_output(cmd)
+        except:
+            pass
+        robot_logger("cmd-{0} o/p={1}".format(str(cmd), output))
+
+        cmd = "ssh {0} \'{1}\'".format((ip_addr, "sudo ps -aef | grep collector | awk '{print $2}' | xargs kill -9")
+        try:
+            code, output = self.run_and_return_rc_and_output(cmd)
+        except:
+            pass
+        robot_logger("cmd-{0} o/p={1}".format(str(cmd), output))
+
+
+    def node_clean_up_from_back-end_command(self, node_type):
+        """ Clean Node Server or Invader from back End
+        """
+        try:
+            for ip, type in eval(str(node_data)).items():
+                if str(type) == "Server":
+                    self.clean_invader(ip)
+                elif str(type) == "Invader":
+                    self.clean_server(ip)
+            return True
+        except:
+            return False
--- test_suite/entry_criteria_test.robot
+++ test_suite/entry_criteria_test.robot
@@ -16,15 +16,34 @@ Test Teardown   Delete All Sessions
 
 *** Test Cases ***
 Invader and Server Cleanup from UI
+        [Tags]    Entry Criteria
+        [Documentation]    Get and Delete available Invader and Server from the PCC
+
         &{data}    Create Dictionary  page=0  limit=50  sortBy=name  sortDir=asc  search=
         ${resp}  Get Request    platina   ${get_node_list}    params=${data}  headers=${headers}
-        Log    \n Status code = ${resp.status_code}    console=yes
-        Log    \n Response = ${resp.json()}    console=yes
+        Log    \n Get Nodes Status code = ${resp.status_code}    console=yes
+        Log    \n Get Nodes Response = ${resp.json()}    console=yes
         Should Be Equal As Strings    ${resp.status_code}    200
         ${status}  ${node_id}  ${node_data}  Get Available Node Data  ${resp.json()}
-        ${status}  Run Keyword And Return Status  Should Be Equal As Strings    ${status}    True
-        Pass Execution If	'{status}' == 'False'
+        Set Suite Variable    ${node_host}    ${node_data}
+        Set Suite Variable    ${node_avail_status}    ${status}
+        Pass Execution If	${node_avail_status}==False    No Nodes are available over PCC
+#	Log    \nDeleting the Node's from UI......     console=yes
+#	:FOR    ${id}    IN    @{node_id}
+#	\    @{data}    Create List    ${id}
+#        \    Log    \nDeleting Node with ID = ${id}    console=yes
+#	\    ${resp}    Post Request    platina    ${delete_node}    headers=${headers}    json=${data}
+#	\    Log    \n Status code = ${resp.status_code}    console=yes
+#	\    Log    \n Response = ${resp.json()}    console=yes
+#	\    Should Be Equal As Strings    ${resp.status_code}    200
+#	\    Sleep    10s
 
 
-
-
+Invader and Server Cleanup from Back-End
+        [Tags]    Entry Criteria
+        [Documentation]    Get and Delete available Invader and Server from back-end
+        Pass Execution If       ${node_avail_status}==False    No Nodes are available over PCC
+	${node_type}    Get Node Type    ${node_host}
+	Log    Node Type Data = ${node_type}    console=yes
+        ${status}    Node Clean up from Back-End Command    ${node_type}
+        Should Be Equal As Strings    ${status}    True    msg=Failed to clean up Data from Node Back End		
